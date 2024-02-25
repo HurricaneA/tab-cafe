24d48057384f66b8eb28f060da1143c972ff56a7
kavishkar_dev <kavishkar@tribz.com>
2024-02-25 10:00:16 +0530

    fixed redirect issue

--- src/util/constants.ts
+++ src/util/constants.ts
@@ -1,2 +1,2 @@
-// export const baseUrl = "http://localhost:4000";
-export const baseUrl = "https://tab-cafe--tab-cafe-be--2jr9q6tjczhk.code.run";
+export const baseUrl = "http://localhost:4000";
+// export const baseUrl = "https://tab-cafe--tab-cafe-be--2jr9q6tjczhk.code.run";
